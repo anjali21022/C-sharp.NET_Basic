2ac3fb5d6e8c5ff6a2dbd455ac4fc9abbf0c70f3
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-01-09 13:16:48 +0530

    Add Project files.

---
+++ Data Types in C sharp/Example to understand Byte(Unsigned) and SByte(Signed) Data Types in C sharp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProject.Data_Types_in_C_sharp
+{
+    internal class Example_to_understand_Byte_Unsigned__and_SByte_Signed__Data_Types_in_C_sharp
+    {
+       static void Main(string[] args)
+        {
+            byte b1 = 65;
+            Console.WriteLine($"Decimal: {b1}");
+            Console.WriteLine($"ASCII Character of {b1} is: {(char)b1}");
+            Console.WriteLine($"Byte Max Value is: {sbyte.MaxValue}  Byte Min Value is: {sbyte.MinValue}");
+            Console.WriteLine($"Byte Size: {sizeof(sbyte)}");
+
+            sbyte b2 = 45;
+            sbyte b3 = -98;
+            Console.WriteLine($"\nDecimal: {b2} {b3}");
+            Console.WriteLine($"ASCII Character of {b2} is: {(char)b2}");
+            Console.WriteLine($"ASCII Character of {b3} is: {(char)b3}");
+            Console.WriteLine($"SByte Max Value is: {sbyte.MaxValue}  SByte Min Value is: {sbyte.MinValue}");
+            Console.WriteLine($"SByte Size: {sizeof(sbyte)}");
+            Console.ReadKey();
+        }
+    }
+}
