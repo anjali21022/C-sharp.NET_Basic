38b649c70c79b9e39a45a621f651af84c19e991a
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-01-09 15:46:44 +0530

    Add files via upload

---
+++ Data Types in C sharp/Example of numeric data type without decimal unsigned integer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Types_in_C_sharp
+{
+    internal class Example_of_numeric_data_type_without_decimal_unsigned_integer
+    {
+        static void Main(string[] args)
+        {
+            ushort num1 = 145;
+            uint num2 = 456;
+            ulong num3 = 723;
+
+            Console.WriteLine($"UShort Min Value is: {ushort.MinValue} UShort Max Value is: {ushort.MaxValue}");
+            Console.WriteLine($"UShort Size is: {sizeof(ushort)} Byte");
+            Console.WriteLine($"Uint Min Value is: {uint.MinValue} Uint Max Value is: {uint.MaxValue}");
+            Console.WriteLine($"Uint Size is: {sizeof(uint)} Byte");
+            Console.WriteLine($"Ulong Min Value is: {ulong.MinValue} Ulong Max Value is: {ulong.MaxValue}");
+            Console.WriteLine($"Ulong Size is: {sizeof(ulong)} Byte");
+            Console.ReadKey();
+        }
+    }
+}
