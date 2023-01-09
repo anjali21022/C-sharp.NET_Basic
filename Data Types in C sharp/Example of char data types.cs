f7324a64e2b1ac92a8b34d9576d0e7ca5ec5fcb5
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-01-09 14:25:16 +0530

    Add Project files.

---
+++ Data Types in C sharp/Example of char data types.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Types_in_C_sharp
+{
+    internal class Example_of_char_data_types
+    {
+        static void Main(string[] args)
+        {
+            char ch = 'A';
+            Console.WriteLine($"Character: {ch}");
+            Console.WriteLine($"Equivalent Character is: {(byte)ch}");
+            Console.WriteLine($"Char Max Value is: {(int)char.MaxValue} Char Min Value is: {(int)char.MinValue}");
+            Console.WriteLine($"Char Size is: {sizeof(char)} Byte");
+            Console.ReadKey();
+        }
+    }
+}
