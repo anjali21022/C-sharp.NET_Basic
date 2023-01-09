0484103f469405e00291403d7fcf21d4c230fe70
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-01-09 11:18:55 +0530

    Add Project files.

---
+++ Methods and Properties of Console Class in C sharp/Example to Understand ReadKey and Read method in C sharp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_and_Properties_of_Console_Class_in_C_sharp
+{
+    internal class Example_to_Understand_ReadKey_and_Read_method_in_C_sharp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Key");
+            int var1 = Console.Read();
+            Console.WriteLine($"ASCII Value of Entered Key is: {var1}");
+
+            Console.WriteLine("Enter Another Key");
+            ConsoleKeyInfo var2 = Console.ReadKey();
+            Console.WriteLine($"\nEntered Key is: {var2.Key} \nASCII: {(int)var2.KeyChar}");
+            Console.ReadKey();
+        }
+    }
+}
