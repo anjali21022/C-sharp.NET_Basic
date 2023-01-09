4d26dd4cac62c79c401feb06685f5c89918ce24d
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-01-09 15:23:56 +0530

    Add files via upload

---
+++ Data Types in C sharp/Example of string data types.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Types_in_C_sharp
+{
+    internal class Example_of_string_data_types
+    {
+        static void Main(string[] args)
+        {
+            string str = "Anjali";
+            var howmanybytes = str.Length * sizeof(char);
+            Console.WriteLine($"String Value is: {str}");
+            Console.WriteLine($"String bytes is: {howmanybytes}");
+            Console.ReadKey();
+        }
+    }
+}
