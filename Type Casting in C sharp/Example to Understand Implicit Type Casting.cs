9cf38569899ac55059a7a3887834f644ade8c0ad
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-01-10 15:20:21 +0530

    Add files via upload

---
+++ Type Casting in C sharp/Example to Understand Implicit Type Casting.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Type_Casting_in_C_sharp
+{
+    internal class Example_to_Understand_Implicit_Type_Casting
+    {
+        static void Main(string[] args)
+        {
+            int numInt = 1500;
+            Type numIntType = numInt.GetType();
+            double numDouble = numInt;
+            Type numDoubleType = numDouble.GetType();
+
+            Console.WriteLine($"numInt Value: {numInt}");
+            Console.WriteLine($"numInt Type: {numIntType}");
+            Console.WriteLine($"Int Size {sizeof(int)} Byte");
+
+            Console.WriteLine($"numDouble Value: {numDouble}");
+            Console.WriteLine($"numDouble Type: {numDoubleType}");
+            Console.WriteLine($"Double Size {sizeof(double)} Bytes");
+
+            Console.ReadKey();
+        }
+    }
+}
