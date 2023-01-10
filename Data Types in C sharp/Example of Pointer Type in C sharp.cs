bc50f979209862d99f6814dd1cba4607ded4b297
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-01-10 10:37:34 +0530

    Add files via upload

---
+++ Data Types in C sharp/Example of Pointer Type in C sharp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Types_in_C_sharp
+{
+    internal class Example_of_Pointer_Type_in_C_sharp
+    {
+        static void Main(string[] args)
+        {
+            unsafe
+            {
+                // declare a variable
+                int number = 10;
+                // store variable number address location in pointer variable ptr
+                int* ptr = &number;
+                Console.WriteLine($"Value :{number}");
+                Console.WriteLine($"Address :{(int)ptr}");
+                Console.ReadKey();
+            }            
+        }
+    }
+}
