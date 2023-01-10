2e3a29fb07b37814f6e207e15dcda7cfd052ad27
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-01-10 10:36:45 +0530

    Add files via upload

---
+++ Data Types in C sharp/Example to Get the Default Values of built-in Data Types in C sharp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Types_in_C_sharp
+{
+    internal class Example_to_Get_the_Default_Values_of_built_in_Data_Types_in_C_sharp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Default Value of Integer: {default(int)}");
+            Console.WriteLine($"Default Value of Character: {default(char)}");
+            Console.WriteLine($"Default Value of Byte: {default(byte)}");
+            Console.WriteLine($"Default Value of Float: {default(float)}");
+            Console.WriteLine($"Default Value of Boolean: {default(Boolean)}");
+            Console.WriteLine($"Default Value of Long: {default(long)}");
+            Console.WriteLine($"Default Value of Double: {default(double)}");
+            Console.ReadKey();
+        }
+    }
+}
