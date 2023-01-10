367594ae8b102a1c750d46d0d446e4bb06660e1c
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-01-10 10:36:02 +0530

    Add files via upload

---
+++ Data Types in C sharp/Example to Get the Minimum and Maximum Range of Values of Built-in Data Types in C sharp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProject.Data_Types_in_C_sharp
+{
+    internal class Example_to_Get_the_Minimum_and_Maximum_Range_of_Values_of_Built_in_Data_Types_in_C_sharp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Float Max Value is:{float.MaxValue} Float Min Value is: {float.MinValue}");
+            Console.WriteLine($"Integer Max Value is:{int.MaxValue} Integer Min Value is: {int.MinValue}");
+            Console.WriteLine($"Double Max Value is:{double.MaxValue} Double Min Value is: {double.MinValue}");
+            Console.WriteLine($"Long Max Value is:{long.MaxValue} Long Min Value is: {long.MinValue}");
+            Console.WriteLine($"Byte Max Value is:{byte.MaxValue} Byte Min Value is: {byte.MinValue}");
+            Console.ReadKey();
+        }
+    }
+}
