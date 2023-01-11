393c5b68c89cffd1c7de86775b20e2c2ae76edc5
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-01-11 10:24:48 +0530

    Add files via upload

---
+++ Literals in C sharp/Example_to_Understand_Integer_Literals(Using int, uint, long, ulong).cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Literals_in_C_sharp
+{
+    internal class Example_to_Understand_Integer_Literals_Using_int__uint__long__ulong_
+    {
+        static void Main(string[] args)
+        {
+            //Integer
+            int a = 100;
+            //Unsigned Integer
+            uint b = 100U;
+            //Long 
+            long c = 100L;
+            //Unsigned Long
+            ulong d = 100UL;
+
+            Console.WriteLine($"Integer: {a}");
+            Console.WriteLine($"Unsigned Integer: {b}");
+            Console.WriteLine($"Long: {c}");
+            Console.WriteLine($"Unsigned Long: {d}");
+            Console.ReadKey();
+        }
+    }
+}
