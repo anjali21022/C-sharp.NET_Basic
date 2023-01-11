e8bc52c4ede0619d1db2c677ff617951465bf1bf
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-01-11 10:24:00 +0530

    Add files via upload

---
+++ Literals in C sharp/Example to Understand Floating-Point Literals.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Literals_in_C_sharp
+{
+    internal class Example_to_Understand_Floating_Point_Literals
+    {
+        static void Main(string[] args)
+        {
+            // Double literal
+            double a = 100; //By Default Floating Point Literal is double
+            //float literal
+            float b = 100.23F; //Suffix with F
+            //decimal literal
+            decimal c = 100.23M; //Suffix with M
+            //double literal
+            double d = 100D; //Suffix with D
+
+            Console.WriteLine($"Double Literal: {a}");
+            Console.WriteLine($"Float Literal: {b}");
+            Console.WriteLine($"Decimal Literal: {c}");
+            Console.WriteLine($"Double Literal: {d}");
+
+            Console.ReadKey();
+        }
+    }
+}
