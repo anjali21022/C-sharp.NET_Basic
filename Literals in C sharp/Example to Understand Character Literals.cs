ddd9e766738113c2e800d81e0f53fcd5763aede2
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-01-11 10:23:30 +0530

    Add files via upload

---
+++ Literals in C sharp/Example to Understand Character Literals.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Literals_in_C_sharp
+{
+    internal class Example_to_Understand_Character_Literals
+    {
+        static void Main(string[] args)
+        {
+            //Character Literal using Single Quote
+            char ch1 = 'A';
+            Console.WriteLine($"Single Quote: {ch1}");
+            //Character Literal using Unicode Representation
+            char ch2 = '\u0045';
+            Console.WriteLine($"Unicode Representation: {ch2}");
+            //Character Literal using Escape Sequence
+            Console.WriteLine("Escape Sequence: Hello\n.NET Tutorials\tWelcome");
+            Console.ReadKey();
+        }
+    }
+}
