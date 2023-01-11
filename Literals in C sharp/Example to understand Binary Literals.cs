6df52629c2dc1369bc5c8a002699d224907dd17f
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-01-11 10:22:41 +0530

    Add files via upload

---
+++ Literals in C sharp/Example to understand Binary Literals.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Literals_in_C_sharp
+{
+    internal class Example_to_understand_Binary_Literals
+    {
+        static void Main(string[] args)
+        {
+            int a = 0b100111101;
+            int b = 0b11110010;
+
+            Console.WriteLine($"Value of a is: {a}");
+            Console.WriteLine($"Value of b is: {b}");
+            Console.WriteLine($"Character Value of a is: {Convert.ToChar(a)}");
+            Console.WriteLine($"Character Value of b is: {Convert.ToChar(b)}");
+
+            Console.ReadKey();
+        }
+    }
+}
