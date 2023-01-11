ca8116731d6d8ae22e02da86da24cafc3e6ae17c
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-01-11 10:23:07 +0530

    Add files via upload

---
+++ Literals in C sharp/Example to understand Boolean Literals.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Literals_in_C_sharp
+{
+    internal class Example_to_understand_Boolean_Literals
+    {
+        static void Main(string[] args)
+        {
+            bool b1 = false;
+            bool b2 = true;
+
+            Console.WriteLine(b1);
+            Console.WriteLine(b2);
+            Console.ReadKey();
+        }
+    }
+}
