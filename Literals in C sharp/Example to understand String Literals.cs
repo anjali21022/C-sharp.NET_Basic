db0eb3a0ad9ab1d54b6c68d0e3db02118259928d
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-01-11 10:24:21 +0530

    Add files via upload

---
+++ Literals in C sharp/Example to understand String Literals.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Literals_in_C_sharp
+{
+    internal class Example_to_understand_String_Literals
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "Dot Net Tutorials";
+            string str2 = @"Dot Net Tutorials";
+
+            string str3 = "Dot\nNet\tTutorials";
+            string str4 = @"Dot\nNet\tTutorials";
+
+            Console.WriteLine(str1);
+            Console.WriteLine(str2);
+            Console.WriteLine(str3);
+            Console.WriteLine(str4);
+
+            Console.ReadKey();
+        }
+    }
+}
