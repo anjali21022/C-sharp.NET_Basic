c632279f0ea253e6767a709a3c5b7959c9ce721f
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-01-12 10:43:07 +0530

    Add files via upload

---
+++ Type Casting in C sharp/Example to Understand Explicit Type Casting.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Type_Casting_in_C_sharp
+{
+    internal class Example_to_Understand_Explicit_Type_Casting
+    {
+        static void Main(string[] args)
+        {
+            double numDouble = 12.34;
+            int numint = numDouble;
+
+            Console.WriteLine($"Original Double Value {numDouble}");
+            Console.WriteLine($"Converted Int Value {numint}");
+
+            Console.ReadKey();
+        }
+    }
+}
+
+//You will not get an output in these program you will get an error which is only the output.
