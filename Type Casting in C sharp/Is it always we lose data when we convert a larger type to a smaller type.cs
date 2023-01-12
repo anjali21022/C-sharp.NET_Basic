65809068190d7bbac54e6871e97084fa009ecdad
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-01-12 10:44:25 +0530

    Add files via upload

---
+++ Type Casting in C sharp/Is it always we lose data when we convert a larger type to a smaller type.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Type_Casting_in_C_sharp
+{
+    internal class Is_it_always_we_lose_data_when_we_convert_a_larger_type_to_a_smaller_type
+    {
+        static void Main(string[] args)
+        {
+            int Intnum1 = 200;
+            byte Bytenum1 = (byte)Intnum1;
+            Console.WriteLine($"Original Value: {Intnum1} Converted Value: {Bytenum1}");
+
+            int Intnum2 = 500;
+            byte Bytenum2 = (byte)Intnum2;
+            Console.WriteLine($"\nOriginal Value: {Intnum2} Converted Value: {Bytenum2}");
+
+            Console.ReadKey();
+        }
+        
+    }
+}
