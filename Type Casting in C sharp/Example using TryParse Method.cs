df7b6d1bbcda8e0a65903d026b553644deb64e92
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-01-12 10:43:32 +0530

    Add files via upload

---
+++ Type Casting in C sharp/Example using TryParse Method.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Type_Casting_in_C_sharp
+{
+    internal class Example_using_TryParse_Method
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "100";
+            bool IsConverted1 = int.TryParse(str1 , out int I1);
+            if (IsConverted1)
+            {
+                Console.WriteLine($"Original String Value: {str1}, Converted Int Value: {I1}");
+            }
+            else
+            {
+                Console.WriteLine($"Try Parse Failed to Convert {str1} to Integer");
+            }
+
+            string str2 = "Hello";
+            bool IsConverted2 = int.TryParse(str2, out int I2);
+            if (IsConverted2)
+            {
+                Console.WriteLine($"Original String Value: {str2}, Converted Int Value: {I2}");
+            }
+            else
+            {
+                Console.WriteLine($"Try Parse Failed to Convert {str2} to Integer");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
