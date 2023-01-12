1568445621ffe4e9429fa851e81d4b48f85b5600
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-01-12 10:43:58 +0530

    Add files via upload

---
+++ Type Casting in C sharp/Example_to_Understand_Explicit_Type_Casting using cast operator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Type_Casting_in_C_sharp
+{
+    internal class Example_to_Understand_Explicit_Type_Casting_using_cast_operator
+    {
+        static void Main(string[] args)
+        {
+            double numDouble = 12.34;
+            int numint = (int)numDouble;
+
+            Console.WriteLine($"Original Double Value {numDouble}");
+            Console.WriteLine($"Converted Int Value {numint}");
+
+            Console.ReadKey();
+        }
+    }
+}
