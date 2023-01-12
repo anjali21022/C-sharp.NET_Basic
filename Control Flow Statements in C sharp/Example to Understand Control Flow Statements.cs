fadc9c6df0ea09ad8e0deb842d7a8d4228234144
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-01-12 12:31:52 +0530

    Add files via upload

---
+++ Control Flow Statements in C sharp/Example to Understand Control Flow Statements.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Control_Flow_Statements_in_C_sharp
+{
+    internal class Control_Flow_Statements
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executing Statement1");
+            Console.WriteLine("Executing Statement2");
+            Console.WriteLine("Executing Statement3");
+            Console.WriteLine("Executing Statement4");
+            Console.WriteLine("Executing Statement5");
+            Console.ReadKey();
+        }
+    }
+}
+//Showing the flow of a program
---
+++ Control Flow Statements in C sharp/Example to Understand Control Flow Statements1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Control_Flow_Statements_in_C_sharp
+{
+    internal class Control_Flow_Statements1
+    {
+        static void Main(string[] args)
+        {
+            if (10 > 5)
+            {
+                Console.WriteLine("Executing Statement1");
+                Console.WriteLine("Executing Statement2");
+            }
+            else
+            {
+                Console.WriteLine("Executing Statement3");
+                Console.WriteLine("Executing Statement4");
+            }
+            Console.WriteLine("Executing Statement5");
+            Console.ReadKey();
+        }
+    }
+}
+//Used if else showing breakage, jumping in program.
