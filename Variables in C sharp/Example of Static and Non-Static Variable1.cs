e03ecc6cd9a21c0c5ba670253109a3da4bcf7f40
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-01-13 11:40:18 +0530

    Add files via upload

---
+++ Variables in C sharp/Example of Static and Non-Static Variable1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables_in_C_sharp
+{
+    internal class Program1
+    {
+        static int x = 100;
+        int y = 200;
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"x value is: {x}");
+            Program1 obj1 = new Program1();
+            Program1 obj2 = new Program1();
+            Console.WriteLine($"y value is: {obj1.y}");
+            Console.WriteLine($"y value is: {obj2.y}");
+            Console.Read();
+        }
+    }
+}
