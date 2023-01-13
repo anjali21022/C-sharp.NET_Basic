b063d2395501abeca823567cbdb2b3a9e257eed8
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-01-13 11:41:35 +0530

    Add files via upload

---
+++ Variables in C sharp/Difference Between Non-Static and Readonly Variables.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables_in_C_sharp
+{
+    internal class Program8
+    {
+        const float PI = 3.14f; //Constant Variable
+        static int x = 100; //Static Variable
+        //We are going to initialize variable y through constructor
+        int y; //Non-Static or Instance Variable
+        readonly int z; //Readonly Variable
+        //Constructor
+        public Program8(int a, int b)
+        {
+            //Initializing non-static variable
+            y = a;
+            //Initializing Readonly variable
+            z = b;
+        }
+        static void Main(string[] args)
+        {
+            //Accessing the static variable without instance
+            Console.WriteLine($"x value: {x}");
+            //Accessing the constant variable without instance
+            Console.WriteLine($"PI value: {PI}");
+            Program8 obj1 = new Program8(300, 45);
+            //Accessing Non-Static variable using instance
+            Console.WriteLine($"obj1 y value: {obj1.y} and Readonly z value: {obj1.z}");
+            obj1.y = 500; //Modifying Non-Static Variable
+            //obj1.z = 400; //Trying to Modify Readonly Variable, Getting Error
+            Console.Read();
+        }
+    }
+}
+//The error at line 34 is the output showing the difference between non-static & readonly variables
