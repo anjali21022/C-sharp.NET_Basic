764f581e2bb10767962069aab203c36e49c4483a
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-01-13 11:42:50 +0530

    Add files via upload

---
+++ Variables in C sharp/Example to Understand Local Variables.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables_in_C_sharp
+{
+    internal class Example_to_Understand_Local_Variables
+    {
+        static void Main(string[] args)
+        {
+            Console.Read();
+        }
+        public void NonStaticBlock()
+        {
+            //By Default, every local variable is going to be non-static variable
+            //The Scope is limited to this method only
+            int x = 100;
+        }
+        public static void StaticBlock()
+        {
+            //By Default, every local variable is going to be a static variable
+            //The Scope is limited to this method only
+            int y = 100;
+        }
+    }
+}
