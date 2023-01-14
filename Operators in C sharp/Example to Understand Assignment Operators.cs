6969293386df2eb0f3c6f66cf1a136f94c4126d0
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-01-14 19:29:04 +0530

    Add files via upload

---
+++ Operators in C sharp/Example to Understand Assignment Operators.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators_in_C_sharp
+{
+    internal class Example_to_Understand_Assignment_Operators
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+
+            // Add Assignment Operator
+            x += 5;
+            Console.WriteLine($"Add Assignment Operator: {x}");
+
+            // Subtract Assignment Operator
+            x = 30;
+            x -= 5;
+            Console.WriteLine($"Subtract Assignment Operator: {x}");
+
+            // Multiply Assignment Operator
+            x = 6;
+            x *= 5;
+            Console.WriteLine($"Multiply Assignment Operator: {x}");
+
+            // Division Assignment Operator
+            x = 15;
+            x /= 5;
+            Console.WriteLine($"Division Assignment Operator: {x}");
+
+            // Modulus Assignment Operator
+            x = 50;
+            x %= 5;
+            Console.WriteLine($"Modulus Assignment Operator: {x}");
+
+            Console.ReadKey();
+        }
+    }
+}
