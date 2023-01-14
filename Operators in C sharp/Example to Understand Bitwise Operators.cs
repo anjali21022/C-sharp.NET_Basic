43ea0a2772507ca67231625d653e6ca40fd1800b
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-01-14 19:29:26 +0530

    Add files via upload

---
+++ Operators in C sharp/Example to Understand Bitwise Operators.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators_in_C_sharp
+{
+    internal class Example_to_Understand_Bitwise_Operators
+    {
+        static void Main(string[] args)
+        {
+            int a = 15;
+            int b = 27;
+            int Result;
+
+            //Bitwise OR Operator (|)
+            Result = a | b;
+            Console.WriteLine($"Bitwise OR Operator (|): {Result}");
+
+            //Bitwise AND Operator (&)
+            Result = a & b;
+            Console.WriteLine($"Bitwise AND Operator (&): {Result}");
+
+            //Bitwise XOR Operator (^)
+            Result = a ^ b;
+            Console.WriteLine($"Bitwise XOR Operator (^): {Result}");
+
+            Console.ReadKey();
+        }
+    }
+}
