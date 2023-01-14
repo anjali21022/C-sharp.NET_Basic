a3256271d7a97ad68010bd1f6bff68fb93a59077
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-01-14 19:30:10 +0530

    Add files via upload

---
+++ Operators in C sharp/Example to Understand Increment and Decrement Operators.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators_in_C_sharp
+{
+    internal class Example_to_Understand_Increment_and_Decrement_Operators
+    {
+        static void Main(string[] args)
+        {
+            int x = 10, y = 20;
+            int z;
+            z = x++ * --y;
+            Console.WriteLine($"x={x}, y={y}, z={z}");
+            Console.ReadKey();
+        }
+    }
+}
