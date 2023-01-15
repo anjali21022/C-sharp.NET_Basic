28586974248db611dce55b33370aef160d46eb89
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-01-15 17:54:44 +0530

    Add files via upload

---
+++ Operators in C sharp/Example_to_Understand_Increment_and_Decrement_Operators using for loop.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators_in_C_sharp
+{
+    internal class Example_to_Understand_Increment_and_Decrement_Operators_using_for_loop
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
