5196cf4455d3ae4741dec8cb2159409f78ae62cc
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-01-16 10:22:17 +0530

    Add files via upload

---
+++ Operators in C sharp/Example to Understand Logical Operators.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators_in_C_sharp
+{
+    internal class Example_to_Understand_Logical_Operators
+    {
+        static void Main(string[] args)
+        {
+            bool x = true;
+            bool y = false;
+            bool z;
+
+            //Logical And Operator (&&)
+            z = x && y;
+            Console.WriteLine("Logical And Operator (&&):" + z);
+
+            //Logical OR Operator (||)
+            z = x || y;
+            Console.WriteLine("Logical OR Operator (||):" + z);
+
+            //Logical Not Operator(!)
+            z = !x;
+            Console.WriteLine("Logical Not Operator(!):" + z);
+
+            Console.ReadKey();
+        }
+    }
+}
