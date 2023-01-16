cebbbef1adb5bee5533a34c2adccd8712f4a3559
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-01-16 10:22:58 +0530

    Add files via upload

---
+++ Operators in C sharp/Example to understand Ternary Operator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators_in_C_sharp
+{
+    internal class Example_to_understand_Ternary_Operator
+    {
+        static void Main(string[] args)
+        {
+            int a = 20, b = 10, res;
+            res = ((a > b) ? a : b);
+            Console.WriteLine("Result = " + res);
+            Console.ReadKey();
+        }
+    }
+}
