59d0004e100615f38b8e6ea56033796f7c416b09
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-01-16 10:22:37 +0530

    Add files via upload

---
+++ Operators in C sharp/Example to Understand Relational Operators.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators_in_C_sharp
+{
+    internal class Example_to_Understand_Relational_Operators
+    {
+        static void Main(string[] args)
+        {
+            bool Result;
+            int a = 3;
+            int b = 6;
+
+            //Equal to (==) Operator
+            Result = a == b;
+            Console.WriteLine($"Equal To Operator: {Result}");
+
+            //Not Equal To (!=) Operator
+            Result = a != b;
+            Console.WriteLine($"Not Equal To Operator: {Result}");
+
+            //Less than (<) Operator
+            Result = a < b;
+            Console.WriteLine($"Less Than Operator: {Result}");
+
+            //Greater than (>) Operator
+            Result = a > b;
+            Console.WriteLine($"Greater Than Operator: {Result}");
+
+            //Less than equal to (<=) Operator
+            Result = a <= b;
+            Console.WriteLine($"Less Than Equal To Operator: {Result}");
+
+            //Greater than equal to (>=) Operator
+            Result = a >= b;
+            Console.WriteLine($"Greater Than Equal To Than Operator: {Result}");
+            
+            Console.ReadKey();
+        }
+    }
+}
