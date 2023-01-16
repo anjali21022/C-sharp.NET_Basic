4c163eeba6f1ab896b4c2ac15b770a94f46598f7
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-01-16 10:23:22 +0530

    Add files via upload

---
+++ Operators in C sharp/Example_to_Understand_Arithmetic_Operators with Operands(values).cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators_in_C_sharp
+{
+    internal class Example_to_Understand_Arithmetic_Operators_with_Operands_values_
+    {
+        static void Main(string[] args)
+        {            
+            int Result;
+
+            //Addition Operator
+            Result = (4 + 2);
+            Console.WriteLine($"Addition Operator: {Result}");
+
+            //Subtraction Operator
+            Result = (4 - 2);
+            Console.WriteLine($"Subtraction Operator: {Result}");
+
+            //Multiplication Operator
+            Result = (4 * 2);
+            Console.WriteLine($"Multiplication Operator: {Result}");
+
+            //Division Operator
+            Result = (4 / 2);
+            Console.WriteLine($"Division Operator: {Result}");
+
+            //Modulus Operator
+            Result = (4 + 2);
+            Console.WriteLine($"Modulus Operator: {Result}");
+
+            Console.ReadKey();
+        }
+    }
+}
