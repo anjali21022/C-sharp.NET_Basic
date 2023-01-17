31e73e6673c55270a1bcc8a2d74fe3cacf82892e
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-01-17 10:14:34 +0530

    Add files via upload

---
+++ Control Flow Statements in C sharp/Example to Understand Control Flow Statements2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Control_Flow_Statements_in_C_sharp
+{
+    internal class Control_Flow_Statements2
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Welcome to .NET");
+            }
+            Console.WriteLine("Welcome to C# .NET");
+            Console.ReadKey();
+        }        
+    }
+}
+//Program using for loop
