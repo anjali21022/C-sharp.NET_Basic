2ebe63f3aa1ef3f1ea8ec87a42db1731ac8579e3
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-01-17 10:14:57 +0530

    Add files via upload

---
+++ Control Flow Statements in C sharp/Example to Understand Control Flow Statements3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Control_Flow_Statements_in_C_sharp
+{
+    internal class Control_Flow_Statements3
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executing Statment1");            
+            Console.WriteLine("Executing Statment2");
+            goto statement4;   //goto label someLabel:
+            Console.WriteLine("Executing Statment3");
+            statement4:    //label:
+            Console.WriteLine("Executing Statment4");
+            Console.WriteLine("Executing Statment5");
+            Console.ReadKey();
+        }        
+    }
+}
+//Used goto Control Statement
