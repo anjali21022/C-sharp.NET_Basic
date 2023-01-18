383d1bbfdfab3155e45bd80043b52e5b8d4260ac
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-01-18 10:19:18 +0530

    Add files via upload

---
+++ If-Else Statements in C sharp/Example to Understand IF-ELSE Statement.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace If_Else_Statements_in_C_sharp
+{
+    internal class IF_ELSE_Statement
+    {
+        static void Main(string[] args)
+        {
+            int Number;
+            Console.WriteLine("Enter a Number");
+            Number = Convert.ToInt32(Console.ReadLine());
+            if (Number % 2 == 0)
+            {
+                Console.WriteLine($"{Number} is an even number");
+            }
+            else
+            {
+                Console.WriteLine($"{Number} is an odd number");
+            }
+            Console.ReadKey();
+        }
+    }
+}
