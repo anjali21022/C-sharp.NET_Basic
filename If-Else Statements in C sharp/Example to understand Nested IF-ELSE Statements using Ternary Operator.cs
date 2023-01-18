4e726c347e33840f0cb2ced77e6f8089928497af
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-01-18 10:20:13 +0530

    Add files via upload

---
+++ If-Else Statements in C sharp/Example to understand Nested IF-ELSE Statements using Ternary Operator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace If_Else_Statements_in_C_sharp
+{
+    internal class Nested_IF_ELSE_Statements_using_Ternary_Operator
+    {
+        static void Main(string[] args)
+        {
+            int a = 10, b = 12, c = 15;
+            int LargestNumber = 0;
+
+            Console.WriteLine($"a={a}, b={b}, c={c}");
+            LargestNumber = (a > b) ? ((a > c) ? (a) : (c)) : ((b > c) ? (b) : (c));
+            Console.WriteLine($"The LargestNumber is {LargestNumber}");
+
+            Console.ReadKey();
+        }
+    }
+}
