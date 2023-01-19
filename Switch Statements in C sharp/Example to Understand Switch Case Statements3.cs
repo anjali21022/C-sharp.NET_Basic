8e4043ed67a0a647d6678ceec105f126de298437
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-01-19 16:49:17 +0530

    Add files via upload

---
+++ Switch Statements in C sharp/Example to Understand Switch Case Statements3.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch_Statements_in_C_sharp
+{
+    internal class Switch_Case_Statements3
+    {
+        static void Main(string[] args)
+        {
+            string Category;
+            //taking topic name
+            string topic = "Inheritance";
+
+            if (topic.Equals("Introduction to C#") ||
+                topic.Equals("Variables") ||
+                topic.Equals("Data Types"))
+            {
+                Category = "Basic";
+            }
+            else if (topic.Equals("Loops") ||
+                topic.Equals("If-else Statement") ||
+                topic.Equals("Jump Statement"))
+            {
+                Category = "Control Flow";
+            }
+            else if (topic.Equals("Inheritance") ||
+                topic.Equals("Polymorphism") ||
+                topic.Equals("Encapsulation") ||
+                topic.Equals("Abstraction"))
+            {
+                Category = "OOPS Concept";
+            }
+            else
+            {
+                Category = "Invalid";
+            }
+            Console.WriteLine($"{topic} Category is of {Category}");
+            Console.ReadKey();
+        }
+    }
+}
