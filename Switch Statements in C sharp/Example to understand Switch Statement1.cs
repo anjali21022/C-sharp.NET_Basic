a18981e61418a01f03dd41a73369d396d8883726
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-01-20 18:00:35 +0530

    Add files via upload

---
+++ Switch Statements in C sharp/Example to understand Switch Statement1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch_Statements_in_C_sharp
+{
+    internal class Switch_Statement1
+    {
+        static void Main(string[] args)
+        {
+            string str = "C#";
+            switch(str)
+            {
+                case "C#":
+                case "Java":
+                case "C":
+                    Console.WriteLine("It is a Programming Language");
+                    break;
+
+                case "MySQL":
+                case "MSSQL":
+                case "Oracle":
+                    Console.WriteLine("It's a Database");
+                    break;
+
+                case "MVC":
+                case "WEB API":
+                    Console.WriteLine("IT's a Freamework");
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid Input");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
