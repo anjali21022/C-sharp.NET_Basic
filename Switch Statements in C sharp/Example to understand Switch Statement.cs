2032031d89f709a57380aeeaebc9e7f1f3401cbd
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-01-20 17:59:59 +0530

    Add files via upload

---
+++ Switch Statements in C sharp/Example to understand Switch Statement.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch_Statements_in_C_sharp
+{
+    internal class Switch_Statement
+    {
+        static void Main(string[] args)
+        {
+            int x = 9;
+            switch (x)
+            {
+                case 1:
+                    Console.WriteLine("Choice is 1");
+                    break;
+                case 2:
+                    Console.WriteLine("Choice is 2");
+                    break;
+                case 9:
+                    Console.WriteLine("Choice is 9");
+                    break;
+                default:
+                    Console.WriteLine("Choice other than 1, 2 and 3");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
