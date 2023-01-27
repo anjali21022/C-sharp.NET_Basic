ccd24add04d8a5d9ca01ca43d0dad3cfde80e7ad
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-01-27 15:56:31 +0530

    Add files via upload

---
+++ If-Else Statements in C sharp/If and Else Block without Curly Braces.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace If_Else_Statements_in_C_sharp
+{
+    internal class If_and_Else_Block_without_Curly_Braces
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            if (a == 10)
+                Console.WriteLine("Hi"); //This Statement belongs to IF Block
+            else
+                Console.WriteLine("Hello"); //This Statement belongs to ELSE Block
+            Console.WriteLine("Bye");
+
+            Console.ReadKey();
+        }
+    }
+}
