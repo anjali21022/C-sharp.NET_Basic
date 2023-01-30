273ffa95e4945effa0c23b6981dc47a5f7f6e993
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-01-30 10:48:10 +0530

    Add files via upload

---
+++ If-Else Statements in C sharp/If Statement without Curly Braces.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace If_Else_Statements_in_C_sharp
+{
+    internal class If_Statement_without_Curly_Braces
+    {
+        static void Main(string[] args)
+        {
+            int Number;
+            Console.WriteLine("Enter a Number");
+            Number = Convert.ToInt32(Console.ReadLine());
+            if (Number > 10)
+                Console.WriteLine($"{Number} is greater than 10");
+            Console.WriteLine("End of Main BlocK");
+            Console.ReadKey();
+        }
+    }
+}
