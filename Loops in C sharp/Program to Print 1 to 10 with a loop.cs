8100edaeb35e4f14c937cee545766e800d6ae883
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-01-31 10:54:55 +0530

    Add files via upload

---
+++ Loops in C sharp/Program to Print 1 to 10 with a loop.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops_in_C_sharp
+{
+    internal class loop1
+    {
+        static void Main(string[] args)
+        {
+            for(int i=1; i<=10; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
