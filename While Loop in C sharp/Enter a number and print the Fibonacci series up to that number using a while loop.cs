f9273f851a0537e166360bfcff10abafe103d533
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-02-02 10:39:00 +0530

    Add files via upload

---
+++ While Loop in C sharp/Enter a number and print the Fibonacci series up to that number using a while loop.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While_Loop_in_C_sharp
+{
+    internal class while_loop2
+    {
+        static void Main(string[] args)
+        {
+            int i, n, j, k;
+            Console.WriteLine("Enter a Number");
+            n = Convert.ToInt32(Console.ReadLine());
+            i = 0;
+            j = 1;
+            Console.Write($"{i} {j}");
+            k = i + j;
+            while(k<=n)
+            {
+                Console.WriteLine($"\n{k}");
+                i = j;
+                j = k;
+                k = i + j;
+            }
+            Console.ReadKey();
+        }
+    }
+}
