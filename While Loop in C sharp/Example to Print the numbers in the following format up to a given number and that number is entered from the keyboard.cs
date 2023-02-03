a03885bf8c9f9a69b2dcf36106f2757fd338b829
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-02-03 10:39:34 +0530

    Add files via upload

---
+++ While Loop in C sharp/Example to Print the numbers in the following format up to a given number and that number is entered from the keyboard.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While_Loop_in_C_sharp
+{
+    internal class while_loop1
+    {
+        static void Main(string[] args)
+        {
+            int i, n;
+            Console.WriteLine("Enter a Number");
+            n = Convert.ToInt32(Console.ReadLine());
+            i = 2;
+            while(i<=n)
+            {
+                Console.WriteLine(i);
+                i = i + 2;
+            }
+            Console.ReadKey();
+        }
+    }
+}
