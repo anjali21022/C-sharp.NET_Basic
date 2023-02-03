a26bd2c89f8d2a5a2e6f14a77303df91eb203840
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-02-03 10:40:14 +0530

    Add files via upload

---
+++ While Loop in C sharp/Example to understand While loop.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While_Loop_in_C_sharp
+{
+    internal class While_loop
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+            while(x<=5)
+            {
+                Console.WriteLine("Value of x:" + x);
+                x++;
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
