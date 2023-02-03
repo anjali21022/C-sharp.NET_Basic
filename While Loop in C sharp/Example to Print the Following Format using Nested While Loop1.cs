08c0607de96cc94c3b9fc96a4e81e8b628e8f27b
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-02-03 10:38:00 +0530

    Add files via upload

---
+++ While Loop in C sharp/Example to Print the Following Format using Nested While Loop1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While_Loop_in_C_sharp
+{
+    internal class Nested_While_Loop1
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            while (a<=5)
+            {
+                int b = 1;
+                while (b <= 5)
+                {
+                    Console.Write(b + " ");
+                    b++;
+                }
+                Console.WriteLine();
+                a++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
