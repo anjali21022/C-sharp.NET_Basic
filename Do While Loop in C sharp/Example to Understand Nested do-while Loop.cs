625e26472e8f2a9834494e5178fd66f7aa51e195
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-02-06 15:34:49 +0530

    Add files via upload

---
+++ Do While Loop in C sharp/Example to Understand Nested do-while Loop.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Do_While_Loop_in_C_sharp
+{
+    internal class Nested_do_while_Loop
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("I am from Outer do-while loop");
+                do
+                {
+                    Console.WriteLine("I am from Inner do-while loop");
+                }
+                while (1 > 10);
+            }
+            while (2 > 10);
+            Console.ReadKey();
+        }
+    }
+}
