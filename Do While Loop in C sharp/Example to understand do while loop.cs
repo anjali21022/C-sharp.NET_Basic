5900fda5e380f699a70a2aeb501898478e7a50b5
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-02-06 15:34:30 +0530

    Add files via upload

---
+++ Do While Loop in C sharp/Example to understand do while loop.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Do_While_Loop_in_C_sharp
+{
+    internal class do_while_loop
+    {
+        static void Main(string[] args)
+        {
+            int number = 1;
+            do
+            {
+                Console.WriteLine(number);
+                number++;
+            }
+            while (number <= 5);
+            Console.ReadKey();
+        }
+    }
+}
