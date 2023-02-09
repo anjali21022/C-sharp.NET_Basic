2c4449565ed8651cfe3efc367d61bacdbcc6a84a
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-02-09 11:43:25 +0530

    Add files via upload

---
+++ For Loop in C sharp/Example to Understand for loop.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For_Loop_in_C_sharp
+{
+    internal class for_loop
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int counter = 1;
+            for (;counter <= number;)
+            {
+                Console.WriteLine(counter);
+                counter++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
