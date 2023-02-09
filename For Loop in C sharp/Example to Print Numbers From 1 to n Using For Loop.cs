a30356c156795a847a706b96dde0af4b7b546903
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-02-09 11:43:07 +0530

    Add files via upload

---
+++ For Loop in C sharp/Example to Print Numbers From 1 to n Using For Loop.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For_Loop_in_C_sharp
+{
+    internal class For_Loop
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            for (int counter = 1; counter <= number; counter++)
+            {
+                Console.WriteLine(counter);
+            }
+            Console.ReadKey();
+        }
+    }
+}
