847f8d802b1b78cd0291c6c6b2312b6c83098db8
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-02-10 10:20:36 +0530

    custom if statement inside for loop

---
+++ For Loop in C sharp/We can write a custom if statement inside for loop.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For_Loop_in_C_sharp
+{
+    internal class custom_if_statement_inside_for_loop
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            for (; ;)
+            {
+                Console.WriteLine("Hello C#");
+                i++;
+                if (i > 5)
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
