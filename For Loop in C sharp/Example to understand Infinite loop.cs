cd7c4d6a83d2ff212044eb8d33823c3a8a527c92
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-02-10 10:19:12 +0530

    Infinite Loops

---
+++ For Loop in C sharp/Example to understand Infinite loop.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For_Loop_in_C_sharp
+{
+    internal class Infinite_loop
+    {
+        static void Main(string[] args)
+        {
+            int number = 10;
+            int i = 1;
+            for(; i <= number;)
+            {
+                Console.WriteLine("Hello");
+            }
+            Console.ReadKey();
+        }
+    }
+}
