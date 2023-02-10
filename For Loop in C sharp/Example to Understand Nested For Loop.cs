45201398fac614d381ac16c775f62c285a8b27af
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-02-10 10:19:50 +0530

    Nested For Loops

---
+++ For Loop in C sharp/Example to Understand Nested For Loop.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For_Loop_in_C_sharp
+{
+    internal class Nested_For_Loop
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine($"Outer For Loop {i}");
+                for(int j = 1; j <= 10; j++)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
