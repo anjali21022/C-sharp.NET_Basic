1ff95e6097a80cfe6664d66cb94e99805adfc058
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-02-14 11:03:36 +0530

    Add files via upload

---
+++ For Loop in C sharp/For Loop Program3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For_Loop_in_C_sharp
+{
+    internal class For_Loop_Program3
+    {
+        static void Main(string[] args)
+        {
+            int number, number1 = 0, number2 = 1, temp;
+            Console.Write("Enter a Number :");
+            number = Convert.ToInt32(Console.ReadLine());
+            if (number >= 1)
+            {
+                Console.Write($"{number1} {number2}");
+                temp = number1 + number2;
+                for (; temp <= number;)
+                {
+                    Console.Write($" {temp}");
+                    number1 = number2;
+                    number2 = temp;
+                    temp = number1 + number2;
+                }
+            }
+            else
+                Console.WriteLine("please enter a number greater than zero");
+            Console.ReadKey();
+        }
+    }
+}
