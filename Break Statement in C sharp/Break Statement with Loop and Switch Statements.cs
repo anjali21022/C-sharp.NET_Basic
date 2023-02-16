a5b3a1aef69aaddca5f2c2c74dd419004b658386
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-02-16 10:54:14 +0530

    Add files via upload

---
+++ Break Statement in C sharp/Break Statement with Loop and Switch Statements.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Break_Statement_in_C_sharp
+{
+    internal class Break_Statement_with_Loop_and_Switch_Statements
+    {
+        static void Main(string[] args)
+        {
+            double[] measurements = { 1, 20, 3, double.NaN };
+            foreach(double measurement in measurements)
+            {
+                switch (measurement)
+                {
+                    case 1:
+                        Console.WriteLine($"Value is {measurement}; too low");
+                        break;
+
+                    case 20:
+                        Console.WriteLine($"Value is {measurement}; too high");
+                        break;                                            
+
+                    case double.NaN:
+                        Console.WriteLine("Invalid");
+                        break;
+
+                   default:
+                        Console.WriteLine($"Value is {measurement}");
+                        break;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
