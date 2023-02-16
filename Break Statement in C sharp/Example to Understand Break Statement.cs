e9a8db9d80d7ffd530310f4a530dfbf58a3d3065
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-02-16 10:54:36 +0530

    Add files via upload

---
+++ Break Statement in C sharp/Example to Understand Break Statement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Break_Statement_in_C_sharp
+{
+    internal class Break_Statement
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"I is: {i}");
+                if ( i == 7)
+                {
+                    break;
+                }                
+            }
+            Console.WriteLine("Out of for-loop");
+            Console.ReadKey();
+        }
+    }
+}
