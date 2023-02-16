71c0d8997052f7b46899e828fa2fd3a32bedac1b
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-02-16 10:55:56 +0530

    Add files via upload

---
+++ Continue Statement in C sharp/Continue Statement inside the nested loop.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Continue_Statement_in_C_sharp
+{
+    internal class Continue_Statement_inside_the_nested_loop
+    {
+        static void Main(string[] args)
+        {
+            for (int outer = 1; outer <= 5; outer++)
+            {
+                Console.WriteLine($"Outer : {outer}");
+                for (int inner = 1; inner <= 5; inner++)
+                {
+                    if (inner == 2)
+                    {
+                        continue;
+                    }
+                    Console.Write($"Inner : {inner}");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();            
+        }
+    }
+}
