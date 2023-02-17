2b980d573d3fb39170adabc97078263653718345
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-02-17 10:47:03 +0530

    Add files via upload

---
+++ Continue Statement in C sharp/Example to Understand Continue Statement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Continue_Statement_in_C_sharp
+{
+    internal class Continue_Statement
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i <= 5; i++)
+            {
+                if (i == 4)
+                {
+                    continue;
+                }
+                Console.WriteLine($"I : {i}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
