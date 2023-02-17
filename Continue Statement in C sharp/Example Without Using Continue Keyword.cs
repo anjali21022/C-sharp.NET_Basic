36cc4ec94445a3324d4af33f7c30bbae08e771e2
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-02-17 10:47:26 +0530

    Add files via upload

---
+++ Continue Statement in C sharp/Example Without Using Continue Keyword.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Continue_Statement_in_C_sharp
+{
+    internal class Example_Without_Using_Continue_Keyword
+    {
+        static void Main(string[] args)
+        {
+            for (int outer = 0; outer <= 7; outer++)
+            {
+                Console.WriteLine($"Outer : {outer}");
+                for (int inner = 0; inner <= 7; inner++)
+                {
+                    if (inner != 3)
+                    {
+                        Console.Write($" Inner : {inner}");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
