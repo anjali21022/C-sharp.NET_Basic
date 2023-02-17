caf0779dcfff3237bd0e1fd5a8a2ec26c058f77d
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-02-17 10:49:17 +0530

    Add files via upload

---
+++ Goto Statement in C sharp/Example to print numbers from 1 to 10 without using the loop.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Goto_Statement_in_C_sharp
+{
+    internal class Example_to_print_numbers_from_1_to_10_without_using_the_loop
+    {
+        static void Main(string[] args)
+        {
+            int count = 1;
+        label_Loop:
+            Console.WriteLine(count);
+            count++;
+            if (count <= 10)
+            {
+                goto label_Loop;
+            }
+            Console.ReadKey();
+        }
+    }
+}
