cfddbb810559e01af983fac111f4d2c84aa7caf9
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-02-20 11:54:40 +0530

    Add files via upload

---
+++ Goto Statement in C sharp/Example to Understand the goto statement.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Goto_Statement_in_C_sharp
+{
+    internal class the_goto_statement
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1st Statement");
+            goto label1;
+            Console.WriteLine("2nd Statement");
+            label1:
+            Console.WriteLine("3rd Statement");
+            Console.WriteLine("End of Main Method");
+            Console.ReadKey();
+        }
+    }
+}
