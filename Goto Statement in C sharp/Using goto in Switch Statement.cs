c1428b6d6169aac81c44dc7d1666a9d163ad732f
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-02-20 11:55:21 +0530

    Add files via upload

---
+++ Goto Statement in C sharp/Using goto in Switch Statement.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Goto_Statement_in_C_sharp
+{
+    internal class Using_goto_in_Switch_Statement
+    {
+        static void Main(string[] args)
+        {
+            int number = 20;
+            switch (number)
+            {
+                case 5:
+                    Console.WriteLine("case 5");
+                    break;
+                case 10:
+                    Console.WriteLine("case 10");
+                    break;
+                case 20:
+                    Console.WriteLine("case 20");
+                    // goto statement transfer
+                    // the control to case 5
+                    goto case 5;
+                default:
+                    Console.WriteLine("No match found");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
