697f2ce57dfb91d7de3728e92327802f57092379
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-02-20 11:55:38 +0530

    Add files via upload

---
+++ Goto Statement in C sharp/Using goto statement inside a Loop.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Goto_Statement_in_C_sharp
+{
+    internal class Using_goto_statement_inside_a_Loop
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                if (i == 5)
+                {
+                    goto label1;
+                }
+                Console.WriteLine(i);
+            }
+            label1:
+                Console.WriteLine("Labeled Statement");
+            
+            Console.ReadKey();
+        }
+    }
+}
