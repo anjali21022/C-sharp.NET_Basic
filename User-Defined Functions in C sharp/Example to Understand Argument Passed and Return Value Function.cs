28742ad3e8e14512ce8174b81f2c3b6744b96d60
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-02-21 12:07:16 +0530

    Add files via upload

---
+++ User-Defined Functions in C sharp/Example to Understand Argument Passed and Return Value Function.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User_Defined_Functions_in_C_sharp
+{
+    internal class Argument_Passed_and_Return_Value_Function
+    {
+        static void Main(string[] args)
+        {
+            int x = 23, y = 45;
+            int Result = Sum(x, y);
+            Console.WriteLine($"Sum is {Result}");
+            Console.ReadKey();
+        }
+        static int Sum(int x, int y)
+        {
+            int Sum = x + y;
+            return Sum;
+        }
+    }
+}
