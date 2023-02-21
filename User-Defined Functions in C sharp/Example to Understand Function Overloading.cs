c4e15d95984fc131726ece5cb58b3f42c4f5ea8c
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-02-21 12:07:56 +0530

    Add files via upload

---
+++ User-Defined Functions in C sharp/Example to Understand Function Overloading.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User_Defined_Functions_in_C_sharp
+{
+    internal class Function_Overloading
+    {
+        static void Main(string[] args)
+        {
+            int a = 12, b = 35, c, d;
+            c = add(a, b);
+            Console.WriteLine($" Sum of {a} and {b} is {c}");
+            d = add(a, b, c);
+            Console.WriteLine($" Sum of {a} and {b} and {c} is {d}");
+            Console.ReadKey();
+        }
+        static int add(int x, int y)
+        {
+            return x + y;
+        }
+        static int add(int x, int y, int z)
+        {
+            return x + y + z;
+        }
+    }
+}
