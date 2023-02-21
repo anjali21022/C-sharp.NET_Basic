f12fa0b36379ef3771bf428bbf1136b12ad9bddd
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-02-21 12:05:07 +0530

    Add files via upload

---
+++ Functions in C sharp/Same Example using Function Method.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functions_in_C_sharp
+{
+    internal class Function_Method
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            x = 10;
+            y = 15;
+            int sum = Add(x, y);
+            Console.WriteLine($"Sum is {sum}");
+            Console.ReadKey();
+        }
+
+        static int Add(int a, int b)
+        {
+            int sum = a + b;
+            return sum;
+        }
+    }
+}
