f8888c1e987cc6fd6fc74f2d40e25ffe6462d65f
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-02-21 12:04:40 +0530

    Add files via upload

---
+++ Functions in C sharp/Example to Understand Functions in C sharp Language.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functions_in_C_sharp
+{
+    internal class Functions_in_C_sharp_Language
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            x = 19;
+            y = 23;
+            int sum = x + y;
+            Console.WriteLine($"Sum is {sum}");
+            Console.ReadKey();
+        }
+    }
+}
