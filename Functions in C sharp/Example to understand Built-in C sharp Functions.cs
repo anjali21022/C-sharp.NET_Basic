ebdd1c6d89083ef81ac0a8eb68cf4bd94f0026c7
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-02-21 12:04:19 +0530

    Add files via upload

---
+++ Functions in C sharp/Example to understand Built-in C sharp Functions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functions_in_C_sharp
+{
+    internal class Built_in_C_sharp_Functions
+    {
+        static void Main(string[] args)
+        {
+            int number = 50;
+            double squareRoot = Math.Sqrt(number);
+            Console.WriteLine($"SquareRoot of {number} is {squareRoot}.");
+            Console.ReadKey();
+        }
+    }
+}
