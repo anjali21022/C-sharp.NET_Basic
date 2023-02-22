ff1949a10dfc321a37060bab44fa25f618232476
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-02-22 10:58:46 +0530

    Add files via upload

---
+++ User-Defined Functions in C sharp/Example to Understand No Arguments Passed and No Return Value Function.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User_Defined_Functions_in_C_sharp
+{
+    internal class No_Arguments_Passed_and_No_Return_Value_Function
+    {
+        static void Main(string[] args)
+        {
+            Sum();
+            Console.ReadKey();
+        }
+        static void Sum()
+        {
+            int x = 13;
+            int y = 4;
+            int sum = x + y;
+            Console.WriteLine(sum); 
+        }
+    }
+}
