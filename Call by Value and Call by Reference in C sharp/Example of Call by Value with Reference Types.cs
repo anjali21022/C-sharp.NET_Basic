34c525e4c4540347354a0f7337ac5a7574f3cc7b
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-02-22 11:02:32 +0530

    Add files via upload

---
+++ Call by Value and Call by Reference in C sharp/Example of Call by Value with Reference Types.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Call_by_Value_and_Call_by_Reference_in_C_sharp
+{
+    internal class Call_by_Value_with_Reference_Types
+    {
+        static void Main(string[] args)
+        {
+            Employee Emp1 = new Employee();
+            Emp1.EmployeeID = 1001;
+            Emp1.Name = "James";
+            Employee Emp2 = Emp1;
+            Emp1.Name = "Smith";
+            Console.WriteLine($"Emp1 Name = {Emp1.Name}");
+            Console.WriteLine($"Emp2 Name = {Emp2.Name}");
+            Console.ReadKey();
+        }
+    }
+    public class Employee
+    {
+        public int EmployeeID;
+        public string Name;
+    }
+}
