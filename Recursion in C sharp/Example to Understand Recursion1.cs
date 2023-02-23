50cce96b7d39e2dadbb878c426a0f925c2463fbd
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-02-23 16:37:24 +0530

    Add files via upload

---
+++ Recursion in C sharp/Example to Understand Recursion1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursion_in_C_sharp
+{
+    internal class Recursion1
+    {
+        static void Main(string[] args)
+        {
+            int number = 5;
+            int Result = fun(number);
+            Console.WriteLine(Result);
+            Console.ReadKey();
+        }
+        static int fun(int n)
+        {
+            int Result = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                Result = Result + i;
+            }
+            return Result;
+        }
+    }
+}
