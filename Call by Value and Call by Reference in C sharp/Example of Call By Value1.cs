ced41bd8c30b44b766fa571c5fe8c6d372f50ee8
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-02-23 16:27:39 +0530

    Add files via upload

---
+++ Call by Value and Call by Reference in C sharp/Example of Call By Value1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Call_by_Value_and_Call_by_Reference_in_C_sharp
+{
+    internal class Call_By_Value1
+    {
+        static void Main(string[] args)
+        {
+            int a = 15;
+            UpdateValue(a);
+            Console.WriteLine(a);
+            Console.ReadKey();
+        }
+        private static void UpdateValue(int b)
+        {
+            b = 30;
+        }
+    }
+}
