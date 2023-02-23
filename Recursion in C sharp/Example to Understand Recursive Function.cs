91cb2dc1a99ae840be6d039f00b65b9030cf1523
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-02-23 16:37:44 +0530

    Add files via upload

---
+++ Recursion in C sharp/Example to Understand Recursive Function.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursion_in_C_sharp
+{
+    internal class Recursive_Function
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main Method Started");
+            fun1(1);
+            Console.WriteLine("Main Method Started");
+            Console.ReadKey();
+        }
+        static void fun1(int n)
+        {
+            Console.WriteLine("Fun1 Started");
+            fun2(1);
+            Console.WriteLine("Fun1 Ended");
+        }
+        static void fun2(int n)
+        {
+            Console.WriteLine("Fun2 Started");
+            fun3(1);
+            Console.WriteLine("Fun2 Ended");
+        }
+        static void fun3(int n)
+        {
+            Console.WriteLine("Fun3 Started");
+            fun4(1);
+            Console.WriteLine("Fun3 Ended");
+        }
+        static void fun4(int n)
+        {
+            Console.WriteLine("Fun4 Started");
+            Console.WriteLine("Fun4 Ended");
+        }
+    }
+}
