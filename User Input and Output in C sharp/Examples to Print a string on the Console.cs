e8bf875910e415cbb75f4d3354c888c20a98c0db
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-02-24 12:58:25 +0530

    Add files via upload

---
+++ User Input and Output in C sharp/Examples to Print a string on the Console.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User_Input_and_Output_in_C_sharp
+{
+    internal class to_Print_a_string_on_the_Console
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hii I am Anjali");
+            Console.ReadKey();
+        }
+    }
+}
