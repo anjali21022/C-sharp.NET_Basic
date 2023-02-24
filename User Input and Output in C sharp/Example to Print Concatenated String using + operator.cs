8bcbb565f787d4219ca67a6960e726ebd7fa8be7
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-02-24 12:56:33 +0530

    Add files via upload

---
+++ User Input and Output in C sharp/Example to Print Concatenated String using + operator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User_Input_and_Output_in_C_sharp
+{
+    internal class Concatenated_String_using___operator
+    {
+        static void Main(string[] args)
+        {
+            int number = 55;
+            Console.WriteLine("Hello " + "C#");
+            Console.WriteLine("Number = " + number);
+        }
+    }
+}
