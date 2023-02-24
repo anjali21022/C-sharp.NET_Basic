84c135c67f3737d3f83cfc075426fb23b0790846
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-02-24 12:56:07 +0530

    Add files via upload

---
+++ User Input and Output in C sharp/Example to Get String Input from User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User_Input_and_Output_in_C_sharp
+{
+    internal class Get_String_Input_from_User
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            Console.Write("Enter a string - ");
+            str = Console.ReadLine();
+            Console.WriteLine($"You entered {str}");
+        }
+    }
+}
