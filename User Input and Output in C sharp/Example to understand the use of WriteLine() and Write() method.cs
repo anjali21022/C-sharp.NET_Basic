c6a91a3ccecf141f33a58b46c0a112e5c8691863
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-02-24 12:58:05 +0530

    Add files via upload

---
+++ User Input and Output in C sharp/Example to understand the use of WriteLine() and Write() method.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User_Input_and_Output_in_C_sharp
+{
+    internal class WriteLine___and_Write___method
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prints on ");
+            Console.WriteLine("New line");
+            Console.Write("Prints on ");
+            Console.Write("Same line");
+        }
+    }
+}
