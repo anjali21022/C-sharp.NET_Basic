d6e5907dd1c9a76a95f18df98628bf2831afe6b4
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-02-24 12:55:17 +0530

    Add files via upload

---
+++ User Input and Output in C sharp/Another way of Reading Numeric Values.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User_Input_and_Output_in_C_sharp
+{
+    internal class Another_way_of_Reading_Numeric_Values
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Integer Value");
+            int intval = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You entered {0}", intval);
+
+            Console.Write("Enter Double Value");
+            double doubleval = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("You entered {0}", doubleval);
+        }
+    }
+}
