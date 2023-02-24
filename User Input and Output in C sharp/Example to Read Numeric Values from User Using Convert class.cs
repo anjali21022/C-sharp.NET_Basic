b8b0dcb3afe770e0f08f0b6f10ba534e51be281d
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-02-24 12:57:21 +0530

    Add files via upload

---
+++ User Input and Output in C sharp/Example to Read Numeric Values from User Using Convert class.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User_Input_and_Output_in_C_sharp
+{
+    internal class Read_Numeric_Values_from_User_Using_Convert_class
+    {
+        static void Main(string[] args)
+        {
+            string UserInput;
+            int intval;
+            double doubleval;
+
+            Console.WriteLine("Enter Integer Value");
+            UserInput = Console.ReadLine();
+            // Converts to integer type
+            intval = Convert.ToInt32(UserInput);
+            Console.WriteLine("You entered {0}", intval);
+
+            Console.Write("Enter double value: ");
+            UserInput = Console.ReadLine();
+            // Converts to double type
+            doubleval = Convert.ToDouble(UserInput);
+            Console.WriteLine("You entered {0}", doubleval);
+        }
+    }
+}
