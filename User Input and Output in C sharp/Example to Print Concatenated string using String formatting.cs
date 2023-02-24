4786db0daa7ddfc257241071bf1eb6a98a3a3c0a
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-02-24 12:57:00 +0530

    Add files via upload

---
+++ User Input and Output in C sharp/Example to Print Concatenated string using String formatting.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User_Input_and_Output_in_C_sharp
+{
+    internal class Concatenated_string_using_String_formatting
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 15, number2 = 20, sum;
+            sum = number1 + number2;
+            Console.WriteLine("{0} + {1} = {2}", number1, number2, sum);
+        }
+    }
+}
