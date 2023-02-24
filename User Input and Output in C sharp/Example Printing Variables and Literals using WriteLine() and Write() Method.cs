e5c5beb038a1b0c6a3a1580fd60e4fd046faf75c
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-02-24 12:55:46 +0530

    Add files via upload

---
+++ User Input and Output in C sharp/Example Printing Variables and Literals using WriteLine() and Write() Method.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User_Input_and_Output_in_C_sharp
+{
+    internal class Printing_Variables_and_Literals_using_WriteLine___and_Write___Method
+    {
+        static void Main(string[] args)
+        {
+            //Printing Variable
+            int number = 10;
+            Console.WriteLine(number);
+            // Printing Literal
+            Console.WriteLine(50.05);
+        }
+    }
+}
