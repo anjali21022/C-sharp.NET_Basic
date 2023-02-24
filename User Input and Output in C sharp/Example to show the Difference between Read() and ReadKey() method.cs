c3e51cc436dd0906b3cd1cd09bf600db9cccdefe
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-02-24 12:57:45 +0530

    Add files via upload

---
+++ User Input and Output in C sharp/Example to show the Difference between Read() and ReadKey() method.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User_Input_and_Output_in_C_sharp
+{
+    internal class Difference_between_Read___and_ReadKey___method
+    {
+        static void Main(string[] args)
+        {
+            int userInput;
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+            Console.WriteLine();
+            Console.Write("Input using Read() - ");
+            userInput = Console.Read();
+            Console.WriteLine("Ascii Value = {0}", userInput);
+        }
+    }
+}
