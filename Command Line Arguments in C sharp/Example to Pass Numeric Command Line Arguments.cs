f9ec6c4f9dbe14a4eecf93c1ee4f948461a8a254
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-02-27 11:04:19 +0530

    Add files via upload

---
+++ Command Line Arguments in C sharp/Example to Pass Numeric Command Line Arguments.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command_Line_Arguments
+{
+    internal class Numeric_Command_Line_Arguments
+    {
+        static void Main(string[] args)
+        {
+            //convert into integer type
+            int argument1 = Convert.ToInt32(args[0]);
+            Console.WriteLine("Argument in Integer Form : " + argument1);
+            //convert into double type
+            double argument2 = Convert.ToDouble(args[1]);
+            Console.WriteLine("Argument in Double Form : " + argument2);
+
+            Console.ReadLine();
+        }
+    }
+}
