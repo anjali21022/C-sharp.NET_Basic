77a3dcd6cde1c48b0be82a1bb315a46fe02721b4
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-02-27 11:04:38 +0530

    Add files via upload

---
+++ Command Line Arguments in C sharp/Passing Command Line Arguments to Main Method.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command_Line_Arguments
+{
+    internal class Passing_Command_Line_Arguments_to_Main_Method
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"First Command Line Argument {args[0]}");
+            Console.WriteLine($"Second Command Line Argument {args[1]}");
+            Console.WriteLine($"Third Command Line Argument {args[2]}");
+            Console.ReadLine();
+        }
+    }
+}
