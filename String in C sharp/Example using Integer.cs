c0c59feff5c6ae7b37d73a69105e00545dd68f2f
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-02-28 11:15:41 +0530

    Add files via upload

---
+++ String in C sharp/Example using Integer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_in_C_sharp
+{
+    internal class Integer
+    {
+        static void Main(string[] args)
+        {
+            int ctr = 0;
+            Console.WriteLine("Loop Started");
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            for (int i = 0; i < 30000000; i++)
+            {
+                ctr = ctr + 1;
+            }
+            stopwatch.Stop();
+            Console.WriteLine("Loop Ended");
+            Console.WriteLine("Loop Exceution Time in MS :" + stopwatch.ElapsedMilliseconds);
+            Console.ReadKey();
+        }
+    }
+}
