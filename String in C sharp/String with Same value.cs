55e5e4a8a4c2a3e09e61dfd08e74f1e8e060ad46
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-02-28 11:16:45 +0530

    Add files via upload

---
+++ String in C sharp/String with Same value.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_in_C_sharp
+{
+    internal class String_with_Same_value
+    {
+        static void Main(string[] args)
+        {
+            string str = "";
+            Console.WriteLine("Loop Started");
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            for (int i = 0; i < 30000000; i++)
+            {
+                str = "DotNet Tutorials";
+            }
+            stopwatch.Stop();
+            Console.WriteLine("Loop Ended");
+            Console.WriteLine("Loop Exceution Time in MS :" + stopwatch.ElapsedMilliseconds);
+            Console.ReadKey();
+        }
+    }
+}
