7a15b07ad1138c92a56b37dc5aa27386a82253c2
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-02-28 11:16:02 +0530

    Add files via upload

---
+++ String in C sharp/Example using StringBuilder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_in_C_sharp
+{
+    internal class StringBuilder
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            Console.WriteLine("Loop Started");
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            for (int i = 0; i < 30000; i++)
+            {
+                stringBuilder.Append("DotNet Tutorials");
+            }
+            stopwatch.Stop();
+            Console.WriteLine("Loop Ended");
+            Console.WriteLine("Loop Exceution Time in MS :" + stopwatch.ElapsedMilliseconds);
+            Console.ReadKey();
+        }
+    }
+}
