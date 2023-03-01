02dd8c3676f5e197c80d0f9df18a2e06502fff37
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-03-01 11:15:06 +0530

    Add files via upload

---
+++ Static vs Non-Static Members in C sharp/Example to Understand Static and Non-Static Constructors.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace StaticNonStaticDemo
+{
+    class Example1
+    {
+        //Static Constructor
+        //Executed only once
+        //First block of code to be executed inside a class
+        //Before Main Method body start executing, this constructor will execute
+        static Example1()
+        {
+            Console.WriteLine("Static Constructor is Called");
+        }
+
+        //Non-Static Constructor
+        //Executed once per object
+        //When we create an instance, this constructor will execute
+        public Example1()
+        {
+            Console.WriteLine("Non-Static Constructor is Called");
+        }
+
+        //Program Execution will start from the Main method
+        //But before executing the Main method body, it will
+        //execute the static constructor
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main Method Execution Start");
+            Example obj1 = new Example();
+            Example obj2 = new Example();
+            Console.WriteLine("Main Method Execution End");
+            Console.ReadLine();
+        }
+    }
+}
