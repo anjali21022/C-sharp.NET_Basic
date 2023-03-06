72aca27b804fc7066c1ace781fa08fb916955d7b
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-03-06 16:27:19 +0530

    Add files via upload

---
+++ Const and Read-Only in C sharp/Example to Understand Read-Only Variables.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Variables_in_C_sharp;
+
+namespace Const_and_Read_Only_in_C_sharp
+{
+    internal class Program
+    {
+        readonly int x; //Readonly Variable
+        static void Main(string[] args)
+        {
+            Program obj1 = new Program();
+            //Accessing Readonly variable using instance
+            Console.WriteLine($"{obj1.x}");
+            Console.Read();
+        }
+    }
+}
