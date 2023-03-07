167be6fbd4181231291ae3cfbc7b2b9a664c9955
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-03-07 12:56:00 +0530

    Add files via upload

---
+++ Properties in C sharp/Example to understand Auto Implemented Properties1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace PropertyDemo
+{
+    public class Student
+    {
+        public int ID;
+        public string Name;
+        public int PassMark;
+    }
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.ID = -100;
+            student.Name = null;
+            student.PassMark = 0;
+            Console.WriteLine($"ID = {student.ID}, Name = {student.Name}, PassMark = {student.PassMark}");
+            Console.ReadKey();
+        }
+    }
+}
