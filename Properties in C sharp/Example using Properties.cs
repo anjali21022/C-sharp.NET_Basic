7df0a6da020e3618e4347683ee3104fa8c450fbb
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-03-08 11:11:20 +0530

    Add files via upload

---
+++ Properties in C sharp/Example using Properties.cs
@@ -0,0 +1,61 @@
+﻿using System;
+namespace PropertyDemo2
+{
+    public class Student
+    {
+        private int _ID;
+        private string _Name;
+        private int _PassMark = 35;
+        public int ID
+        {
+            set
+            {
+                if (value < 0)
+                {
+                    throw new Exception("ID value should be greater than zero");
+                }
+                _ID = value;
+            }
+            get
+            {
+                return _ID;
+            }
+        }
+        public string Name
+        {
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("Name should not be empty");
+                }
+                _Name = value;
+            }
+            get
+            {
+                return string.IsNullOrEmpty(_Name) ? "No Name" : _Name;
+            }
+        }
+        public int PassMark
+        {
+            get
+            {
+                return _PassMark;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.ID = 101;
+            student.Name = "Pranaya";
+
+            Console.WriteLine($"ID = {student.ID}");
+            Console.WriteLine($"Name = {student.Name}");
+            Console.WriteLine($"Pass Mark = {student.PassMark}");
+            Console.ReadKey();
+        }
+    }
+}
