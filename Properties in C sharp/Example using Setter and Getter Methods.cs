a0ec182859458c5adb53c0dfe55833444b6beb37
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-03-08 11:11:41 +0530

    Add files via upload

---
+++ Properties in C sharp/Example using Setter and Getter Methods.cs
@@ -0,0 +1,56 @@
+﻿using System;
+namespace PropertyDemo1
+{
+    public class Student
+    {
+        private int _ID;
+        private string _Name;
+        private int _PassMark = 35;
+        public void SetID(int ID)
+        {
+            if (ID < 0)
+            {
+                throw new Exception("ID value should be greater than zero");
+            }
+            _ID = ID;
+        }
+        public int GetID()
+        {
+            return _ID;
+        }
+        public void SetName(string Name)
+        {
+            if (string.IsNullOrEmpty(Name))
+            {
+                throw new Exception("Name should not be empty");
+            }
+            _Name = Name;
+        }
+        public string GetName()
+        {
+            if (string.IsNullOrEmpty(_Name))
+            {
+                return "No Name";
+            }
+            return _Name;
+        }
+        public int GetPassMark()
+        {
+            return _PassMark;
+        }
+    }
+    class Program3
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.SetID(101);
+            student.SetName("Pranaya");
+
+            Console.WriteLine($"ID = {student.GetID()}");
+            Console.WriteLine($"Name = {student.GetName()}");
+            Console.WriteLine($"Pass Mark = {student.GetPassMark()}");
+            Console.ReadKey();
+        }
+    }
+}
