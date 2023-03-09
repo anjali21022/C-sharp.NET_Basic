9a2db2bc928d7190a388ffa51bd87600dfaccb74
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-03-09 12:34:16 +0530

    Add files via upload

---
+++ Override Equals Method in C sharp/Equals method and == Operator with Enum Type.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Override_Equals_Method_in_C_sharp
+{
+    internal class Equals_method_and____Operator_with_Enum_Type
+    {
+        public class Program
+        {
+            public static void Main()
+            {
+                Direction direction1 = Direction.East;
+                Direction direction2 = Direction.East;
+                Console.WriteLine(direction1 == direction2);
+                Console.WriteLine(direction1.Equals(direction2));
+                Console.ReadKey();
+            }
+        }
+        public enum Direction
+        {
+            East = 1,
+            West = 2,
+            North = 3,
+            South = 4
+        }
+    }
+}
