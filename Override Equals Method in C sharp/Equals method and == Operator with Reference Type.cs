bfae38a2509416d211e1eea5595a24b2f828522e
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-03-09 12:34:59 +0530

    Add files via upload

---
+++ Override Equals Method in C sharp/Equals method and == Operator with Reference Type.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Override_Equals_Method_in_C_sharp
+{
+    internal class Equals_method_and____Operator_with_Reference_Type
+    {
+        public static void Main()
+        {
+            Customer C1 = new Customer();
+            C1.FirstName = "Pranaya";
+            C1.LastName = "Rout";
+            Customer C2 = C1;
+            Console.WriteLine($"C1 == C2: {C1 == C2}");
+            Console.WriteLine($"C1.Equals(C2): {C1.Equals(C2)}");
+            Console.ReadKey();
+        }
+    }
+    public class Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
+
