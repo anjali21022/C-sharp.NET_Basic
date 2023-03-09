3a124a58592254296d19b153f7785e8cd22330d7
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-03-09 12:33:58 +0530

    Add files via upload

---
+++ Override Equals Method in C sharp/Difference Between the “==” Operator and the Equals() Method.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Override_Equals_Method_in_C_sharp
+{
+    internal class Difference_Between_the______Operator_and_the_Equals___Method
+    {
+        public static void Main()
+        {
+            int Number1 = 10;
+            int Number2 = 10;
+            Console.WriteLine($"Number1 == Number2: {Number1 == Number2}");
+            Console.WriteLine($"Number1.Equals(Number2): {Number1.Equals(Number2)}");
+            Console.ReadKey();
+        }
+    }
+}
