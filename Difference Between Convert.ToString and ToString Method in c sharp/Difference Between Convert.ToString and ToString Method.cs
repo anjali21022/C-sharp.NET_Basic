3b1c747df522e642c65797049eba6797930d01b2
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-03-10 10:36:50 +0530

    Add files via upload

---
+++ Difference Between Convert.ToString and ToString Method in c sharp/Difference Between Convert.ToString and ToString Method.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Difference_Between_Convert.ToString_and_ToString_Method_in_c_sharp
+{
+    internal class Difference_Between_Convert
+    {
+        public static void Main()
+        {
+            Customer C1 = null;
+            C1.ToString();
+            Console.ReadLine();
+        }
+    }
+    public class Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
