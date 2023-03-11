5e7b0d067bc61535ac43bc9ded65027bffc4c6a2
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-03-11 20:56:36 +0530

    Add files via upload

---
+++ Checked and Unchecked Keywords in C sharp/Example to Understand Checked Keyword.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checked_and_Unchecked_Keywords_in_C_sharp
+{
+    internal class Example_to_Understand_Checked_Keyword
+    {
+        static void Main(string[] args)
+        {
+            int a = 2147483647;
+            int b = 2147483647;
+            int c = checked(a + b);
+            Console.WriteLine(c);
+            Console.ReadLine();
+        }
+    }
+}
