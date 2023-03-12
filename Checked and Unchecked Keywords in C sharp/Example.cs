c77e81942dfa9eb457843122dcfc9db31c0f5220
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-03-12 12:54:50 +0530

    Add files via upload

---
+++ Checked and Unchecked Keywords in C sharp/Example.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checked_and_Unchecked_Keywords_in_C_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(int.MaxValue);
+            Console.ReadLine();
+        }
+    }
+}
