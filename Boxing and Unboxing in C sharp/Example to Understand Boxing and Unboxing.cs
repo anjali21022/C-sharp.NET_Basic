b7d4b041bebcf14a16dfdd639230647a3eec5715
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-03-14 10:37:32 +0530

    Add files via upload

---
+++ Boxing and Unboxing in C sharp/Example to Understand Boxing and Unboxing.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boxing_and_Unboxing_in_C_sharp
+{
+    internal class Example_to_Understand_Boxing_and_Unboxing
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            object y = x; //Boxing
+            int z = (int)y; //Unboxing
+        }
+    }
+}
